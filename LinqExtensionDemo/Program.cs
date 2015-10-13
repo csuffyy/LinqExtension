cf6a27b2767ba2c00b97a6ebe04570c4e15e0de0
csuffyy <csu-lzy@163.com>
2015-10-13 17:43:20 +0800

    1、初始版； 2、增加了C#6.0的语法。

---
+++ LinqExtensionDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start:");
+
+            try
+            {
+                Test();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Test()
+        {
+            var users = Enumerable.Range(0, 100).Select((x, i) => new User
+            {
+                Name = "lzy" + i,
+                Age = 18 + i,
+                RegisteredTime = DateTimeOffset.Now.AddDays(i)
+            });
+
+            //C#6.0
+            var contirions = new Dictionary<string, List<string>>()
+            {
+                ["Name"] = new List<string> {"lzy9"},
+                ["Age"] = new List<string> {"<110", ">100"},
+                ["RegisteredTime"] = new List<string> {">=2016/01/18"},
+            };
+
+            var query = users.AsQueryable().Query(contirions);
+
+            foreach (var user in query)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
---
+++ LinqExtensionDemo/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LinqExtension
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Name { get; set; } = "lzy";
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; } = 18;
+
+        /// <summary>
+        /// 是否被禁用
+        /// </summary>
+        public bool IsLocked { get; set; } = false;
+
+        /// <summary>
+        /// 注册时间
+        /// </summary>
+        public DateTimeOffset RegisteredTime { get; set; } = DateTimeOffset.Now;
+
+        public override string ToString()
+        {
+            return $"{Name}-{Age}-{RegisteredTime}";
+        }
+    }
+}
